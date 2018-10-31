9af3dda06ba01135994213c219bb6ea35dd97cc4
techbunny <c-ompany@outlook.jp>
2018-10-31 12:45:25 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :favorites
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :likeds, through: :reverses_of_favorite, source: :user
 end
